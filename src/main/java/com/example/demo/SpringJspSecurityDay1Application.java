0e95808c7dbc2a4d96f48bad8758b48089f4160b
chandrakant sir <cakant70@gmail.com>
2023-04-10 11:45:43 +0530

    day

---
+++ src/main/java/com/example/demo/SpringJspSecurityDay1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJspSecurityDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJspSecurityDay1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	String myfun()
+	{
+		return "home.jsp";
+	}
+	
+	
+	
+}
